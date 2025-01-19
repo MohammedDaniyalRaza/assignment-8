9682252fc2a5b774119c7225a862d9be71e5e850
MohammedDaniyalRaza <160598244+MohammedDaniyalRaza@users.noreply.github.com>
2025-01-19 12:39:33 +0500

    Initial commit

--- app/query/route.ts
+++ app/query/route.ts
@@ -5,9 +5,9 @@
 // async function listInvoices() {
 // 	const data = await client.sql`
 //     SELECT invoices.amount, customers.name
-//     FROM invoices
-//     JOIN customers ON invoices.customer_id = customers.id
-//     WHERE invoices.amount = 666;
+//        FROM invoices
+//        JOIN customers ON invoices.customer_id = customers.id
+//        WHERE invoices.amount = 666;
 //   `;
 
 // 	return data.rows;
